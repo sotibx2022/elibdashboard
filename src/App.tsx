096db014990195d8dbbb4ac4bbdd1ee3c3f17fbd
Binaya <sbinayaraj@gmail.com>
2024-09-10 15:24:52 +0300

    tailwind and shadcn added to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { Button } from './components/ui/button'
 function App() {
   return (
    <div>
     <h1>Main Page</h1>
+    <Button>Click Me</Button>
    </div>
   )
 }
